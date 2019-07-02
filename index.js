e23400fe1e59051212e0661fa59abda6664c6107
David Sodré <davidsodre_ba@hotmail.com>
2019-07-02 14:44:09 -0300

    Correct - Question 4 of challenge - Javascritp Introduction - RocketSeat

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+//Question 01
 //console.log(question01());
+
+//Question 02
 //pairs(3,30);
 
-var skills =  ["Javascript", "ReactJS", "React Native"];
-doHaveSkill(skills);
+//Quetion 03
+//var skills =  ["Javascript", "ReactJS", "React Native"];
+//doHaveSkill(skills);
 
+//Question 04
+var studyYears = 4;
+experience(studyYears);
 
 /*
 This question is simple and easy because use one object and data 
@@ -39,5 +46,27 @@ This question require a search on vector to find a skill. If the
 Skill Javascrit exist on structure, return true else falso.
 */ 
 function doHaveSkill(skills){
+    //the function indexOf return -1 if the element is not found.
     console.log((skills.indexOf("Javascript")==-1)?false:true);
-}
+};
+
+/*
+This question requite to return the experiente level since years study
+*/
+function experience(studyYears){
+    if(studyYears>0 && studyYears<=1){
+        console.log("Beginer");
+    }    
+    if(studyYears>1 && studyYears<=3){
+        console.log("Intermediary");
+    }
+    if(studyYears>3 && studyYears<=6){
+        console.log("Advanced");
+    }
+    if(studyYears>=7){
+        console.log("Jedi Master");
+    }
+    if(studyYears<0 || studyYears>100){
+        console.log("Invalid Value");
+    }
+};
