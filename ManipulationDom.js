ed2bec916b58159a695f39df9b8ca335aae73800
David Sodré <davidsodre_ba@hotmail.com>
2019-07-08 09:57:33 -0300

    Question 04 - using before list create an input element and a button and add to list the name typed on input after clear content - Javascritp DOM Manipulation - RocketSeat V2

--- ManipulationDom.js
+++ ManipulationDom.js
@@ -1,13 +1,29 @@
 //question 01
 var appElement = document.querySelector("#app"); //busca a div com id='app' | get the div's id='app'
 var btnElement = document.createElement('button'); //cria um elemento botão | crete a new button element
+var inputElement = document.createElement('input');
+
+inputElement.setAttribute('id','name');
+inputElement.setAttribute('type','text');
+inputElement.setAttribute('name','nome');
+
+btnAddElement = document.createElement('button');
+btnElement.setAttribute('class', 'botao');
+var txtButtonElement = document.createTextNode('ADD');
+btnAddElement.appendChild(txtButtonElement);
 
 btnElement.setAttribute('class', 'botao'); //seta a classe botão ao button | set class botao to button
 var textElement = document.createTextNode('Generate Square'); //cria o rótulo do botão | Create label of button
-
 btnElement.appendChild(textElement); //acrescenta o rotulo ao botão | append label on button
+btnAddElement.appendChild(txtButtonElement);
 appElement.appendChild(btnElement); //acrescenta o botão na div app | append button on app div
 
+appElement.appendChild(inputElement);
+appElement.appendChild(btnAddElement);
+
+btnAddElement.onclick = function(){
+    add();
+}
 btnElement.onclick = function () { //configura a ação do botão criado com uma função | setup a funcion action to button 
     squareGenerate(); //função que desenha um quadrado | funcion to draw square
 };
@@ -47,4 +63,15 @@ function getRandomColor() {
     }
     return color;
 }
-   
+
+//#### QUESTAO 4 ####
+function add(){
+    var List = document.querySelector('ul'); // Pega a ul
+    var inputElement = document.getElementById('name');//pega o input pelo id
+    var itemElement = document.createElement('li'); //cria uma nova li
+    var nameElement = document.createTextNode(inputElement.value); //cria o texto digitado no li
+    itemElement.appendChild(nameElement); //adiciona o nome no li
+    List.appendChild(itemElement); //coloca o li dentro do ul
+    inputElement.value = ""; //deixa o input vazio
+}
+//#### QUESTAO 4 END #####
